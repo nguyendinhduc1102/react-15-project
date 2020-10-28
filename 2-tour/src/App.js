d61ff31dd6fcd8e86230c21e4f1e01899f8e99f3
nguyendinhduc1102 <nguyendinhduc29041999@gmail.com>
2020-10-28 10:57:03 +0700

    Completed project 2-tours

--- 2-tour/src/App.js
+++ 2-tour/src/App.js
@@ -5,13 +5,43 @@ const url = 'https://course-api.netlify.app/api/react-tours-project'
 function App() {
   const [loading, setLoading] = useState(true)
   const [tours, setTours] = useState([])
+
+  const removeTour = (id,removeTour)=>{
+    const newTours = tours.filter((tour)=>tour.id !== id);
+    setTours(newTours)
+  }
+  const fetchTours = async ()=>{
+    setLoading(true)
+    try {
+      const response = await fetch(url)
+      const tours = await response.json()
+      setLoading(false)
+      setTours(tours) 
+    } catch (error) {
+      setLoading(false)
+      console.log(error);
+    }
+    
+    
+  }
+  useEffect(() => {
+    fetchTours();
+  }, [])
   if (loading) {
     return <main>
       <Loading />
     </main>
   }
+  if (tours.length === 0) {
+    return <main className="title">
+          <h2>No tours left</h2>
+          <button className="btn" onClick={fetchTours} >Refresh</button>
+    </main>
+  }
   return (
-    <h2>Tours project setup</h2>
+    <main>
+      <Tours tours={tours} removeTour={removeTour}/>
+    </main>
   );
 }
 
