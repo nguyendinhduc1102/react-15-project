5517fda594e76db299d15408c7185e5b6594580b
nguyendinhduc1102 <nguyendinhduc29041999@gmail.com>
2020-10-28 10:00:17 +0700

    complated project 1

--- 1-birday-reminder/src/App.js
+++ 1-birday-reminder/src/App.js
@@ -3,9 +3,14 @@ import data from  './data'
 import List from './List'
 
 function App() {
-  return (
-    <h1>Project setup</h1>
-  );
+  const [people, setPeople] = useState(data)
+  return (<main>
+  <section className="container">
+      <h3>{people.length} birthdays today</h3>
+      <List people={people}/>
+      <button onClick={()=>setPeople([])}>Clear all</button>
+    </section>
+  </main>);
 }
 
 export default App;
