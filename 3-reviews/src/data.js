364a6a54bca12dfe92657e9bbae321d74bf5408b
nguyendinhduc1102 <nguyendinhduc29041999@gmail.com>
2020-10-28 11:55:26 +0700

    Completed project 3-review

---
+++ 3-reviews/src/data.js
@@ -0,0 +1,62 @@
+const reviews = [{
+  "id": 1,
+  "name": "Klaus Philipsson",
+  "job": "Environmental Tech",
+  "image": "https://robohash.org/corruptiquamofficia.jpg?size=50x50&set=set1",
+  "text": "nulla justo aliquam quis turpis eget elit sodales scelerisque mauris sit amet eros suspendisse accumsan tortor quis turpis sed ante"
+}, {
+  "id": 2,
+  "name": "Brian Gore",
+  "job": "Geological Engineer",
+  "image": "https://robohash.org/exercitationemporroassumenda.jpg?size=50x50&set=set1",
+  "text": "nulla nisl nunc nisl duis bibendum felis sed interdum venenatis turpis enim"
+}, {
+  "id": 3,
+  "name": "Kristen Dicke",
+  "job": "Technical Writer",
+  "image": "https://robohash.org/saepeetsint.bmp?size=50x50&set=set1",
+  "text": "ut massa quis augue luctus tincidunt nulla mollis molestie lorem"
+}, {
+  "id": 4,
+  "name": "Sada McConville",
+  "job": "Web Designer IV",
+  "image": "https://robohash.org/quiarationequasi.jpg?size=50x50&set=set1",
+  "text": "molestie lorem quisque ut erat curabitur gravida nisi at nibh"
+}, {
+  "id": 5,
+  "name": "Jethro Stiddard",
+  "job": "Analog Circuit Design manager",
+  "image": "https://robohash.org/etearumautem.png?size=50x50&set=set1",
+  "text": "velit donec diam neque vestibulum eget vulputate ut ultrices vel augue vestibulum ante ipsum primis"
+}, {
+  "id": 6,
+  "name": "Kelley Blue",
+  "job": "Legal Assistant",
+  "image": "https://robohash.org/nihilnumquamvel.png?size=50x50&set=set1",
+  "text": "donec ut dolor morbi vel lectus in quam fringilla rhoncus mauris enim leo rhoncus sed"
+}, {
+  "id": 7,
+  "name": "Darrell Johann",
+  "job": "Programmer I",
+  "image": "https://robohash.org/quasquidemvero.jpg?size=50x50&set=set1",
+  "text": "amet eleifend pede libero quis orci nullam molestie nibh in lectus pellentesque at nulla suspendisse potenti cras"
+}, {
+  "id": 8,
+  "name": "Shaine Coulthurst",
+  "job": "Budget/Accounting Analyst III",
+  "image": "https://robohash.org/utquiaconsectetur.bmp?size=50x50&set=set1",
+  "text": "quis turpis sed ante vivamus tortor duis mattis egestas metus aenean fermentum donec ut"
+}, {
+  "id": 9,
+  "name": "Sher Gorges",
+  "job": "Marketing Manager",
+  "image": "https://robohash.org/hicdolorecorrupti.jpg?size=50x50&set=set1",
+  "text": "nisi at nibh in hac habitasse platea dictumst aliquam augue quam sollicitudin"
+}, {
+  "id": 10,
+  "name": "Clemmy Meddick",
+  "job": "Accounting Assistant I",
+  "image": "https://robohash.org/ametaest.jpg?size=50x50&set=set1",
+  "text": "parturient montes nascetur ridiculus mus vivamus vestibulum sagittis sapien cum sociis natoque penatibus et magnis dis parturient"
+}]
+export default reviews
