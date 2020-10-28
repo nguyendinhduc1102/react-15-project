dc13ad87e7c75c369441512dfcdd28a8aaa04051
nguyendinhduc1102 <nguyendinhduc29041999@gmail.com>
2020-10-28 14:29:54 +0700

    Accordion project setup

---
+++ 4-accordion/src/data.js
@@ -0,0 +1,42 @@
+const questions = [{
+  "id": 1,
+  "title": "Tango Lesson, The",
+  "info": "Other repair of aneurysm"
+}, {
+  "id": 2,
+  "title": "Galaxina",
+  "info": "Other skin graft to lip and mouth"
+}, {
+  "id": 3,
+  "title": "Noise",
+  "info": "Percutaneous abdominal drainage"
+}, {
+  "id": 4,
+  "title": "Dinner at Eight",
+  "info": "Closed reduction of nasal fracture"
+}, {
+  "id": 5,
+  "title": "Abbott and Costello Meet the Killer, Boris Karloff",
+  "info": "Other urethroscopy"
+}, {
+  "id": 6,
+  "title": "Gorillas in the Mist",
+  "info": "Other puncture of artery"
+}, {
+  "id": 7,
+  "title": "Escape Me Never",
+  "info": "Other diagnostic procedures on urethra and periurethral tissue"
+}, {
+  "id": 8,
+  "title": "City of No Limits, The (la ciudad sin límites, En)",
+  "info": "Microscopic examination of specimen from skin and other integument, culture"
+}, {
+  "id": 9,
+  "title": "Moulin Rouge",
+  "info": "Open reduction of fracture without internal fixation, carpals and metacarpals"
+}, {
+  "id": 10,
+  "title": "Talladega Nights: The Ballad of Ricky Bobby",
+  "info": "Incision of duodenum"
+}]
+export default questions
