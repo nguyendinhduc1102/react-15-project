ed50a29e78105d178257227174550fc739f67b01
nguyendinhduc1102 <nguyendinhduc29041999@gmail.com>
2020-10-29 22:09:17 +0700

    Cocktail project setup

---
+++ 15-cocktails/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Home from './pages/Home'
+import About from './pages/About'
+import SingleCocktail from './pages/SingleCocktail'
+import Error from './pages/Error'
+import Navbar from './components/Navbar'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Coctail project</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ 15-cocktails/src/context.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const AppProvide = ({children}) => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default AppProvide
