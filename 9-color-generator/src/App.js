ebcc9dd66e2531123cc3ed9aaacf7f6f47d36e1c
nguyendinhduc1102 <nguyendinhduc29041999@gmail.com>
2020-10-29 09:43:13 +0700

    Completed generator project

--- 9-color-generator/src/App.js
+++ 9-color-generator/src/App.js
@@ -1,9 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import SingleColor from './SingleColor'
-
+import Values from 'values.js'
 function App() {
+  const [color, setColor] = useState('')
+  const [error, setError] = useState(false)
+  const [list, setList] = useState(new Values('#f15025').all(10))
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+    
+    try {
+      
+      let colors = new Values(color).all(10)
+      setList(colors)
+    } catch (error) {
+      setError(true)
+      console.log(error);
+    }
+  }
   return (
-    <h1>Color generator</h1>
+    <>
+    <section className='container'>
+      <h3>color generator</h3>
+      <form onSubmit={handleSubmit}>
+        <input type='text' value={color} 
+          onChange={(e)=>setColor(e.target.value)} 
+          placeholder='#f1502'
+          className={`${error? 'error':null}`}/>
+        <button type='submit' className='btn'>submit</button>
+      </form>
+    </section>
+    <section className='color'>
+      {list.map((color,index)=>{
+        console.log(color);
+        return <SingleColor key={index} {...color} index={index} hexColor={color.hex}/>
+      })}
+    </section>
+    </>
   );
 }
 
