3b1e95a4dd436f008fdf74c143568a514e974f89
nguyendinhduc1102 <nguyendinhduc29041999@gmail.com>
2020-10-29 08:24:36 +0700

    Completed project Lorem ipsum

--- 8-lorem-ipsum/src/App.js
+++ 8-lorem-ipsum/src/App.js
@@ -5,7 +5,14 @@ function App() {
   const [text, setText] = useState([])
   const handleSubmit = (e)=>{
     e.preventDefault()
-    console.log("hello");
+    let amount = parseInt(count)
+    if (count <= 0) {
+      amount =  1
+    }
+    if (count > 4) {
+      amount = 4
+    }
+    setText(data.slice(0,amount))
   }
   return (
     <section className='section-center'>
@@ -14,8 +21,14 @@ function App() {
         <label htmlFor='amount'>
           paragraph: 
         </label>
-        <input type='number' name='amount' id='amount' />
+        <input type='number' name='amount' id='amount' value={count} onChange={(e)=>setCount(e.target.value)}/>
+        <button types='submit' className='btn'>generate</button>
       </form>
+      <article className='lorem-text'>
+        {text.map((item,index)=>{
+          return <p key={index}>{item}</p>
+        })}  
+      </article>
     </section>
   );
 }
